c637542718563214705faa46fa8a6c2269cecba1
Zouari Omar <zouariomar20@gmail.com>
2024-05-08 18:06:44 +0100

    add the arduino.ino file

--- project/arduino/controller/controller.ino
+++ project/arduino/controller/controller.ino
@@ -8,44 +8,37 @@
 //? ----------------------- NOTE SECTION DECLARATION PART -----------------------
 /*
 * if we can use "Serial.begin(115200)", use it. it give a quick communication response
+! we don't use the switch_joystick_button
 */
 
 //? -------------------- INCLUDE PROTOTYPE DECLARATION PART --------------------
 #include <LiquidCrystal_I2C.h>
 
 //? -------------- LCD MSG(S) PROTOTYPE DECLARATION PART --------------
-#define LCD_MSG00  "Welcome To Hollow Vessels";
+#define LCD_MSG00  "Welcome To Hollow Vessels"
 
 //* init the lcd
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 //? ------------------ PINS PROTOTYPE DECLARATION PART ------------------
-//* define the Rx, Ry and switch_joystick_button pins
+//* define the Rx, Ry pins
 const int Rx_pin           = A0;
 const int Ry_pin           = A1;
-const int interaction_pin  = 2;
 
-//* define the 3 mvs switch button pins
+//* define the 2 mvs switch button pins
 const int jump_pin         = 13;
 const int dash_pin         = 12;
-const int slide_pin        = 11;
+
+//* define the interaction switch button pin
+const int interaction_pin  = 11;
 
 //* define the pause/resume  switch button pin
 const int pr_pin           = 10;
 
 //? -------------- PUBLIC VARS PROTOTYPE DECLARATION PART --------------
-//* define the R/L and the interaction vars
-int right,
-    left,
-    interaction;
-
-//* define the 3 moves vars
-int jump,
-    dash,
-    slide;
-
-//* define the pause/resume var
-int pr;
+//* define the X/Y vars
+int x,
+    y;
 
 //* define the input delay
 int dt = 500;
@@ -53,53 +46,61 @@ int dt = 500;
 
 //? ----------------------- FUNCTIONS PROTOTYPE DEV PART -----------------------
 void setup() {
-    //* set the serial seed
+    //* set the @serial_monitor_reading_speed
     Serial.begin(9600);
 
-    //* init the lcd
+    //? -- init the LCD --
     lcd.init();
     lcd.backlight();
 
-    //* set Rx_pin, Ry_pin, interaction_pin in INPUT mode
+    //? -- INPUT pins mode declaration part --
+    //* set Rx_pin, Ry_pin in IM
     pinMode(Rx_pin,          INPUT);
     pinMode(Ry_pin,          INPUT);
-    pinMode(interaction_pin, INPUT);
 
-    //* set Rx_pin, Ry_pin, interaction_pin in INPUT mode
-    pinMode(jump_pin,   INPUT);
-    pinMode(dash_pin,   INPUT);
-    pinMode(slide_pin,  INPUT);
+    //* set Rx_pin, Ry_pin in IM
+    pinMode(jump_pin,        INPUT_PULLUP);
+    pinMode(dash_pin,        INPUT_PULLUP);
 
-    //* set the pr_pin in INPUT mode
-    pinMode(pr_pin,     INPUT);
+    //* set the interaction_pin in IM
+    pinMode(interaction_pin, INPUT_PULLUP);
 
-    //* set interaction_pin in HIGH mode
+    //* set the pr_pin in IM
+    pinMode(pr_pin,          INPUT_PULLUP);
+
+    //* set interaction_pin in HIGH read mode
     digitalWrite(interaction_pin, HIGH);
+
+    lcd.setCursor(0, 0);
+    lcd.print(LCD_MSG00);
 }
 
 void loop() {
     //* scroll the lcd txt to the left
-    //lcd.scrollDisplayLeft();
+    lcd.scrollDisplayLeft();
 
-    //* detect the Rx_pin actions
-    //x = analogRead(Rx_pin);
+    //* detect the @joystick_directions using the Rx/Ry pins (8 direction)
+    x = analogRead(Rx_pin);
 
     //* detect the Ry_pin actions
-    //y = analogRead(Ry_pin);
+    y = analogRead(Ry_pin);
 
-    //! testing part
-    int direction;
+    ////Serial.println(x, y);
 
-    //* detect the interaction_pin actions
-    interaction = digitalRead(interaction_pin);
+    //* detect the 2 moves pins actions    :: active in LOW mode
+    if (!digitalRead(jump_pin))
+        Serial.println("is_jump");
 
-    //* detect the 3 moves actions
-    jump  = digitalRead(jump_pin);
-    dash  = digitalRead(dash_pin);
-    slide = digitalRead(slide_pin);
+    if (!digitalRead(dash_pin))
+        Serial.println("is_dash");
 
-    //* detect the pause/resume actions
-    pr = digitalRead(pr_pin);
+    //* detect the interaction_pin actions :: active in LOW mode
+    if (!digitalRead(interaction_pin))
+        Serial.println("is_interact");
+
+    //* detect the pr_pin actions          :: active in LOW mode
+    if (!digitalRead(pr_pin))
+        Serial.println("is_pause");
 
     //* test the execution of the joystick
     /*
